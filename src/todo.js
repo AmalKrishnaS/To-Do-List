7311b989359c917c206aee7a10e608a285f25078
AmalKrishna <121586027+AmalKrishnaS@users.noreply.github.com>
2025-04-26 14:24:47 +0000

    feat: add toDo class with editing methods

---
+++ src/todo.js
@@ -0,0 +1,30 @@
+class toDo {
+    constructor(title, description, dueDate, priority, checklist) {
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.priority = priority;
+        this.checklist = checklist;
+    }
+
+    rename(title) {
+        this.title = title;
+    }
+
+    editDescription(description) {
+        this.description = description;
+    }
+
+    changeDueDate(dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    changePriority(priority) {
+        this.priority = priority;
+    }
+
+    changeChecklist(checklist) {
+        this.checklist = checklist;
+    }
+
+}
