e6c07ef4a0ff336ae0ed597e32c0cc4b20fbcc0b
AmalKrishna <121586027+AmalKrishnaS@users.noreply.github.com>
2025-05-28 02:40:47 +0000

    feat: add new project functionality
    - Show add project dialog on '+' button click
    - Moved project and todo dialogs inside .container for proper layout
    - Styled add project dialog for consistent appearance
    - Added getProject and setProject functions to handle localStorage

--- src/app.js
+++ src/app.js
@@ -3,14 +3,37 @@ import { createProject } from "./project";
 import { displayProjects } from "./render";
 import {displayTodos} from "./render.js";
 
+function getProject() {
+    const projects = JSON.parse(localStorage.getItem('projects'));
+    return projects; 
+}
+
+function setProject(projects) {
+    localStorage.setItem('projects', JSON.stringify(projects));
+}
+
 document.addEventListener('DOMContentLoaded', () => {
-    if (localStorage.getItem('projects') === null) {
-        const defaultProject = createProject('Default');
-        displayProjects([defaultProject]);
+    const projects = getProject();
+    if (projects === null) {
+        projects.push({name:'Default', todos: {}})
+        displayProjects(projects[0]);
     } 
     else {
-        const defaultProject = JSON.parse(localStorage.getItem('projects'));
-        displayProjects(defaultProject);
-        displayTodos(defaultProject[0]);
+        displayProjects(projects);
+        displayTodos(projects[0]);
     }
+})
+
+const projectDialog = document.querySelector('.project-dialog');
+const addProjectButton = document.querySelector('.add-project');
+const projectForm = document.querySelector('.project-form'); 
+
+addProjectButton.addEventListener('click', (e) => {
+    projectDialog.showModal();    
+});
+
+const cancelProjectButton = document.querySelector('.cancel-project-button');
+cancelProjectButton.addEventListener('click', (e) => {
+    e.preventDefault();
+    projectDialog.close();    
 })
